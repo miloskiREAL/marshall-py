2b73e95568bdd7bc090560fe2daac228d0a0d088
miloskiREAL <milos@gtabesthomes.com>
2025-02-14 14:56:17 -0500

    solved lesson 4

--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,13 @@
 # Lesson 4
+import math
+print ("section one")
+sec1 = len(input())
+print ("section two")
+sec2 = len(input())
+print ("section three")
+sec3 = len(input())
+total = sec1 + sec2 + sec3
+cans = 12 * (math.ceil(total / 12))
+remain = cans % total 
+cost = 14.95 * math.ceil(total / 12)
+print("you will need\n",cans, " cans \n",remain," remainder\n$",cost," cost")
