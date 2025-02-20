8a669c298c53bcff04a6b309c276ccef7af4dff6
miloskiREAL <milos@gtabesthomes.com>
2025-02-20 18:46:38 -0500

    lesson13 complete

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,14 @@
 # Lesson 13
+month = int(input("enter the month\n"))
+date = int(input("enter the day\n"))
+if month > 2 :
+    print("after")
+elif month < 2 :
+    print("before")    
+else :
+    if date < 18 :
+        print("before")  
+    elif date > 18 :
+        print("after")
+    else :
+        print("special")
