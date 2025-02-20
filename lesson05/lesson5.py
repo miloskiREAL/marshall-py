d634d2f0b18f76793e4e13af62a9ad5958833604
miloskiREAL <milos@gtabesthomes.com>
2025-02-20 13:35:51 -0500

    lesson5 solved

--- lesson05/lesson5.py
+++ lesson05/lesson5.py
@@ -1,1 +1,13 @@
 # Lesson 5
+print("how much money did you start with")
+total = float(input())
+print("what cookies did you sell")
+cookies = list(input().lower())
+totSold = len(cookies)
+bcook = cookies.count("b")
+scook = cookies.count("c")
+bprofit = float((2 * bcook) - (0.75 * bcook))
+sprofit = float((1.25 * scook) - (0.5 * scook))
+tprofit = bprofit + sprofit
+total += tprofit
+print(f"You sold {totSold} cookies \nmade ${tprofit} profit \nand now have ${total} total")
