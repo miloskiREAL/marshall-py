bc349f470a5312cc3c44b83ae07225536325de1b
miloskiREAL <milos@gtabesthomes.com>
2025-02-20 18:22:05 -0500

    solved 10

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,8 @@
 # Lesson 10
+number = input("enter the number\n")
+if int(number[0]) == 8 or int(number[0]) == 9 :
+    if int(number[1]) == int(number[2]) :
+        if int(number[0]) == 8 or int(number[0]) == 9 :
+            print("scammer")
+else :
+    print("not scammer")
