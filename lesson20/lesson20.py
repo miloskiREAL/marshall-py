05f529ff817dc843a04eecc4394c3f6d3325b554
miloskiREAL <milos@gtabesthomes.com>
2025-02-21 17:42:20 -0500

    20 done

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,1 +1,16 @@
 # Lesson 20
+n = 6
+n2 = 1
+check = 0
+while n <= 10000:
+    while n2 < n:
+        if n % n2 == 0:
+            check += n2
+        n2 += 1
+    if check == n:
+        print(n)
+    n += 1
+    n2 = 1
+    check = 0
+
+            
