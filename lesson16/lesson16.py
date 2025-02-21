7559241fd3e3e24006a8e3aa0c8012fe83788c29
miloskiREAL <milos@gtabesthomes.com>
2025-02-20 20:00:04 -0500

    lesson 16 complete

--- lesson16/lesson16.py
+++ lesson16/lesson16.py
@@ -1,1 +1,10 @@
 # Lesson 16
+for n in range(1,51):
+    if n % 3 == 0 and n % 5 == 0 :
+        print("fizzbuzz")
+    elif n % 5 == 0 :
+        print("buzz")
+    elif n % 3 == 0 :
+        print("fizz")
+    else :
+        print(n)
