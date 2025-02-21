4c2d30c0784b5a285eb635123a2894436f1f95e4
miloskiREAL <milos@gtabesthomes.com>
2025-02-20 21:37:20 -0500

    lesson 12 complete

--- lesson12/lesson12.py
+++ lesson12/lesson12.py
@@ -1,1 +1,13 @@
 # Lesson 12
+ang1 = int(input("enter an angle\n"))
+ang2 = int(input("enter an angle\n"))
+ang3 = int(input("enter an angle\n"))
+if (ang1 + ang2 + ang3) == 180 :
+    if ang1 == ang2 == ang3 :
+        print("equilateral")
+    elif ang1 == ang2 or ang2 == ang3 or ang1 == ang3:
+        print("isoceles")
+    else :
+        print("scalene")
+else:
+    print("not a triangle")
