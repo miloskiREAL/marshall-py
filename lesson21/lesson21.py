9aa4c3ee09423df06f17a3501d33307330e07cda
miloskiREAL <milos@gtabesthomes.com>
2025-02-21 17:23:57 -0500

    21 done

--- lesson21/lesson21.py
+++ lesson21/lesson21.py
@@ -1,1 +1,19 @@
 # Lesson 21
+maxF = 0
+nMax = 0
+factors = 0
+n = 1
+n2 = 1
+num = int(input("enter a number "))
+while n <= num:
+    while n2 <= n:
+        if n % n2 == 0:
+            factors += 1
+        n2 += 1
+    if maxF < factors:
+        nMax = n
+        maxF = factors
+    n += 1
+    n2 = 1
+    factors = 0
+print(nMax)
