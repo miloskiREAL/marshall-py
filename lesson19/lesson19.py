f421cf251120a373bbc7aafb08441ead9e3975d8
miloskiREAL <milos@gtabesthomes.com>
2025-02-20 20:23:21 -0500

    lesson 19 complete

--- lesson19/lesson19.py
+++ lesson19/lesson19.py
@@ -1,1 +1,14 @@
 # Lesson 19
+num = int(input("enter a number\n"))
+fCount = 0
+n = 1
+while n <= num:
+    if fCount > 2:
+        break
+    elif num % n == 0:
+        fCount += 1
+    n += 1
+if fCount == 2:
+    print("prime")
+else :
+    print("not prime")
