81e565155704eeed4cea79350b4a98a1b1c48607
miloskiREAL <milos@gtabesthomes.com>
2025-02-20 23:18:48 -0500

    23 done

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,12 @@
 # Lesson 23
+num = 0
+sumN = 0
+counter = 0
+while True:
+    num = input("enter a number or x to exit\n")
+    if num == 'x':
+        break
+    else:
+        sumN += int(num)
+        counter += 1
+print(f"average is {sumN/counter}")
