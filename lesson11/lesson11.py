e26b25ee93661feb8f4dd097b660153869728149
miloskiREAL <milos@gtabesthomes.com>
2025-02-20 21:12:13 -0500

    lesson 11 complete

--- lesson11/lesson11.py
+++ lesson11/lesson11.py
@@ -1,1 +1,18 @@
 # Lesson 11
+coords = input("enter coords\n")
+coords = coords.split()
+def cast(value):
+   return int(value)
+coords = list(map(cast, coords))
+if coords[0] == 0 or coords [1] == 0:
+    print("not in a quadrant")
+elif coords[0] > 0:
+    if coords[1] > 0:
+        print("1")
+    else :
+        print("4") 
+else :
+    if coords[1] > 0:
+        print("2")
+    else:
+        print("3")
