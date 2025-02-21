4b5fac0249a3b3963735202ff22f8c5c2b30faef
miloskiREAL <milos@gtabesthomes.com>
2025-02-21 13:54:37 -0500

    9 done

--- lesson09/lesson9.py
+++ lesson09/lesson9.py
@@ -1,1 +1,31 @@
 # Lesson 9
+import random
+choices = ["rock", "paper", "scissors"]
+userChoice = ""
+
+while userChoice != "x" :
+    print("enter your choice")
+    userChoice = input()
+    botChoice = random.choice(choices)
+    if userChoice not in choices:
+        print("not available")
+    else :
+        if userChoice == botChoice :
+            print("draw")
+        elif userChoice == "scissors" :
+            if botChoice == "rock":
+                print("lost")
+            else:
+                print("won")
+        elif userChoice == "rock":
+            if botChoice == "paper":
+                print("lost")
+            else:
+                print("won")
+        else:
+            if botChoice == "scissors":
+                print("lost")
+            else:
+                print("won")
+        print(f"{botChoice} vs {userChoice}")
+
