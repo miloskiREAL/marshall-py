7cdea9255d7a5dee29f9e51119bd8698ab3e2c4f
miloskiREAL <milos@gtabesthomes.com>
2025-02-20 22:51:46 -0500

    lesson 24 complete

--- lesson24/lesson24.py
+++ lesson24/lesson24.py
@@ -1,1 +1,11 @@
 # Lesson 24
+lName = ''
+name = ''
+while True:
+    name = input("enter a name\n")
+    if name == "x":
+        break
+    else:
+        if len(name) > len(lName):
+            lName = name
+print(lName)
