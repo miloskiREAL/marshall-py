e5f4ee8e4c52a53a8bbc51702959a4d643f36c21
miloskiREAL <milos@gtabesthomes.com>
2025-02-21 19:10:50 -0500

    22 done

--- lesson22/lesson22.py
+++ lesson22/lesson22.py
@@ -1,1 +1,17 @@
 # Lesson 22
+num = int(input("enter a number: "))
+n = 2
+fibn2 = 0
+fibn1 = 1
+fibNth = 0
+if num == 1:
+    print("1")
+elif num == 0:
+    print("0")
+else:
+    while n <= num:
+        fibNth = fibn1 + fibn2
+        fibn2 = fibn1
+        fibn1 = fibNth
+        n += 1
+    print(fibNth)
