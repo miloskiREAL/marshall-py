e6566bfe8c46cc85bf0d8a44caa88ebf94b9190b
miloskiREAL <milos@gtabesthomes.com>
2025-02-21 22:49:12 -0500

    15 done

--- lesson15/lesson15.py
+++ lesson15/lesson15.py
@@ -1,1 +1,27 @@
 # Lesson 15
+conditionVar = True
+password = "hello"
+while conditionVar:
+    userIn = input("guess the password ")
+    
+    if userIn == password:
+        print("CORRECT")
+        conditionVar = False
+    else:
+        print("INCORRECT")
+
+print("\n")
+
+userIn = input("search for fruits: ")
+
+fruits = ["apple", "banana", "orange", "pineapple"]
+
+found = False
+
+for fruit in fruits:
+    if fruit == userIn:
+        print(f"{userIn} was found!")
+        found = True
+
+if not found:
+    print(f"{userIn} was not found")
