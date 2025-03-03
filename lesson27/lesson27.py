f79c37164a5bec765a439301feeeac42527d78f8
miloskiREAL <milos@gtabesthomes.com>
2025-03-03 13:59:43 -0500

    lesson 27 SOLVED

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,7 @@
 # Lesson 27
+dirty = list(input("enter a phrase "))
+clean = ""
+for n in dirty: 
+    if (n <= "z" and n >= "a") or (n <= "Z" and n >= "A") or n == " ":
+        clean += n
+print(clean)
