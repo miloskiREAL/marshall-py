eda797462528e11a11323fe4a9a85208726e6261
miloskiREAL <milos@gtabesthomes.com>
2025-03-04 14:27:17 -0500

    lesson 31 SOLVED

--- lesson31/lesson31.py
+++ lesson31/lesson31.py
@@ -1,1 +1,12 @@
 # Lesson 31
+word1 = input("enter word: ")
+word2 = input("enter word: ")
+if len(word1) == len(word2):
+    aList = sorted(word1)
+    bList = sorted(word2)
+    if aList == bList:
+        print("yep")
+    else:
+        print("nope")
+else:
+    print("nope")
