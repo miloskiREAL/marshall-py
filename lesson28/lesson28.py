a1b45cea141371af22c6cd2ecc230612c33cb8aa
miloskiREAL <milos@gtabesthomes.com>
2025-03-04 13:59:57 -0500

    lesson 28 SOLVED

--- lesson28/lesson28.py
+++ lesson28/lesson28.py
@@ -1,1 +1,12 @@
 # Lesson 28
+text = input("enter a word: ").lower()
+anagram = input("enter the anagram: ").lower()
+if not text or not anagram:
+    print("no text provided")
+elif text[0] != anagram[-1]:
+    print("not an anagram")
+else:
+    if text == anagram[::-1]:
+        print("yep")
+    else: 
+        print("nope")
