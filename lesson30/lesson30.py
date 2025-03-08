5f38f5a431987526cc1cd108433b3f27592c579e
miloskiREAL <milos@gtabesthomes.com>
2025-03-08 12:53:05 -0500

    30 done

--- lesson30/lesson30.py
+++ lesson30/lesson30.py
@@ -1,1 +1,9 @@
 # Lesson 30
+n = int(input("enter a number: "))
+output = ""
+for i in range(n):
+    if (i + 1) % 2 == 0:
+        output += "0"
+    else: 
+        output += "1"
+    print(output)
