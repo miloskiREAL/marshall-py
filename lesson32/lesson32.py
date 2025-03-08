640f9b2c4f55d31f0efdae9a13353c5309e0a200
miloskiREAL <milos@gtabesthomes.com>
2025-03-08 13:55:46 -0500

    32 done

--- lesson32/lesson32.py
+++ lesson32/lesson32.py
@@ -1,1 +1,12 @@
 # Lesson 32
+word1 = input("enter a word: ")
+word2 = input("enter a word: ")
+temp = set(word1 + word2)
+final = []
+for n in temp:
+    if n in word1 and n in word2:
+        final.append(n)
+print(sorted(final))
+
+
+
