f1c8469d01efb484051db05c50004baa33bb9b39
miloskiREAL <milos@gtabesthomes.com>
2025-03-08 14:56:17 -0500

    33 done

--- lesson33/lesson33.py
+++ lesson33/lesson33.py
@@ -1,1 +1,14 @@
 # Lesson 33
+import math
+n = int(input("how many values: "))
+values = []
+for i in range(n):
+    values.append(int(input("enter a value: ")))
+mean = sum(values) / len(values)
+length = len(values)
+values.sort()
+if length % 2 != 0:
+    median = values[length // 2]
+else: 
+    median = (values[(length // 2)]+ values[(length // 2) - 1]) / 2
+print(f"median is {median} mean is {mean}")
