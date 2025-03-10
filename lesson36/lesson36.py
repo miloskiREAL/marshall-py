d628334b6ff0062ec1365cb29d00f0898124a993
miloskiREAL <milos@gtabesthomes.com>
2025-03-09 23:24:08 -0400

    36 done

--- lesson36/lesson36.py
+++ lesson36/lesson36.py
@@ -1,1 +1,16 @@
 # Lesson 36
+num = int(input("enter a number: "))
+def factors():
+    factorList = []
+    for n in range(num):
+        if num % (n + 1) == 0:
+            factorList.append(n + 1)
+    return factorList
+def isPrime():
+    return len(factors()) == 2
+factorList = factors()
+prime = isPrime()
+print(f"the factors are {factorList} is this number prime? {prime}")
+
+
+
