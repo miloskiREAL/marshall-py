9fe9f6b384a8685d2d583c92f71270731ed92172
miloskiREAL <milos@gtabesthomes.com>
2025-03-09 23:52:09 -0400

    40 done

--- lesson40/lesson40.py
+++ lesson40/lesson40.py
@@ -1,1 +1,15 @@
 # Lesson 40
+num = int(input("enter a number: "))
+def factors():
+    factorList = []
+    for n in range(1, i + 1):
+        if i % n == 0:
+            factorList.append(n)
+    return factorList
+def isPrime():
+    return len(factors()) == 2
+primeFact = []
+for i in range(1, num + 1):
+    if num % i == 0 and isPrime():
+        primeFact.append(i)
+print(primeFact)
