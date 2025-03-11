cf7d3bfce69c8cb6799eeffb0647b49f4f15cab0
miloskiREAL <milos@gtabesthomes.com>
2025-03-11 18:27:17 -0400

    43 done

--- lesson43/lesson43.py
+++ lesson43/lesson43.py
@@ -1,1 +1,22 @@
 # Lesson 43
+def rdupes(a_list):
+    setVer = set(a_list)
+    return list(setVer)
+print(rdupes([1,1,1,2,2,2,3,3,3,4,4,4]))
+def uniLet(a_list):
+    result = set()
+    for word in a_list:
+        temp = set(word)
+        result |= temp
+    return result
+testB = ["hello", "goodbye", "world", "mr. park"]
+print(uniLet(testB))
+def intersect(a_list):
+    if a_list:
+        result = a_list[0]
+
+        for example in a_list[1:]:
+            result = result & example
+        return len(result)
+test = [{1,2,3,7}, {3,4,5,7}, {5,6,7,8}]
+print(intersect(test))
