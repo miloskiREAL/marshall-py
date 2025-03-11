c504f2572c48922c0089cebc6bd816ee6d662119
miloskiREAL <milos@gtabesthomes.com>
2025-03-10 23:32:13 -0400

    44 done

--- lesson44/lesson44.py
+++ lesson44/lesson44.py
@@ -1,1 +1,12 @@
 # Lesson 44
+word = str(input("enter word: ")).lower()
+keys = sorted(list(set(word)))
+freq = {}
+for n in keys:
+    count = 0
+    if n.isalpha():
+        for i in sorted(list(word)):
+            if i == n:
+                count += 1
+        freq.update({n : count})
+print(freq)
