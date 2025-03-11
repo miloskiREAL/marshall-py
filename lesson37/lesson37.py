6d8465b901932ae96aa85edcdfaf748a62b37edb
miloskiREAL <milos@gtabesthomes.com>
2025-03-10 22:40:07 -0400

    37 done

--- lesson37/lesson37.py
+++ lesson37/lesson37.py
@@ -1,1 +1,17 @@
 # Lesson 37
+def compress():
+    compressed = ""
+    for n in sorted(list(set(word))):
+        count = 0
+        compressed += n
+        for i in word:
+            if n == i:
+                count += 1
+        compressed += str(count)
+        
+    if len(compressed) >= len(word):
+        return word
+    else:
+        return compressed
+word = str(input("enter a word: ")).lower()
+print(compress())
