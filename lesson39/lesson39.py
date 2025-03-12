0ec873df7b94b2afa3a59f6ef4e6a445d7d97de0
miloskiREAL <milos@gtabesthomes.com>
2025-03-12 17:32:48 -0400

    39 done

--- lesson39/lesson39.py
+++ lesson39/lesson39.py
@@ -1,1 +1,8 @@
 # Lesson 39
+def gcd(num, num2):
+    for n in range(min(num, num2), 0, -1):
+        if num % n == 0 and num2 % n == 0:
+            return n
+num = int(input(("enter a number: ")))
+num2 = int(input(("enter a number: ")))
+print(f"the gcd is {gcd(num, num2)}")
