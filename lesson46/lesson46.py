c5b65ec745d91ac98511088a5a267645ef51f36d
miloskiREAL <milos@gtabesthomes.com>
2025-03-12 21:39:04 -0400

    46 done

--- lesson46/lesson46.py
+++ lesson46/lesson46.py
@@ -1,1 +1,15 @@
 # Lesson 46
+longChain = {}
+def col(num):
+    count = 0
+    while num != 1:
+        if num % 2 == 0:
+            num /= 2
+            count += 1
+        else:
+            num = 3 * num + 1
+            count += 1
+    return count
+for num in range(1, 1000000):
+    longChain[col(num)] = num
+print(f"the maximum chain is {max(longChain)} with a value of {longChain[max(longChain)]}")
